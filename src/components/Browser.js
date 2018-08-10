ae2c977f502067ed1cd63e46945cca86ad419f74
April Cound <musicwithapril@gmail.com>
2018-08-10 04:25:35 -0700

    imported drizzle react components, attempted to debug state issue with console logs

--- src/components/Browser.js
+++ src/components/Browser.js
@@ -3,7 +3,7 @@ import { object } from 'prop-types';
 import Web3 from 'web3';
 import kittyABI from '../contracts/KittyCoreABI.json';
 import { CONTRACT_NAME, CONTRACT_ADDRESS } from '../config';
-// import { ContractForm } from './Form'
+// import { ContractData, ContractForm } from "drizzle-react-components";
 
 class Browser extends Component {
   
@@ -23,6 +23,18 @@ class Browser extends Component {
       contractName: CONTRACT_NAME,
       web3Contract: kittyContract,
     });
+    
+    console.log("context:", this.context.drizzle)
+    var currentState = this.context.drizzle.store.getState()
+    console.log("store:", currentState)
+
+
+
+    // const dataKey = this.context.drizzle.contracts.CryptoKitties.methods.getKitty.cacheCall()
+    // console.log("datakey:", dataKey)
+
+    // const data = state.contracts.CryptoKitties.methods.getKitty[dataKey].value
+    // console.log(data)
   }
 
   render() {
@@ -32,18 +44,28 @@ class Browser extends Component {
           Kitty Browser
         </h1>
 
+
         {/* Input to type in the kitty ID here */}
           <h4>
             Kitty ID:
           </h4>
-          {/* <form>
-            <input type="text" />
-            <input type="submit" value="FIND KITTY" />
-          </form>   */}
-          {/* <ContractData contract="kittyContract" method="storedData" /> */}
-          {/* <ContractForm contract="kittyContract" method="getKitty" /> */}
+        
+          {/* <ContractForm
+              contract="kittyContract"
+              method="getKitty"
+              
+            /> */}
 
         {/* Display Kitty info here */}
+
+        {/* <strong>Birth Time</strong>:{" "}
+              <ContractData
+                contract="kittyContract"
+                method="getKitty"
+
+          /> */}
+
+
       </div>
     );
   }
